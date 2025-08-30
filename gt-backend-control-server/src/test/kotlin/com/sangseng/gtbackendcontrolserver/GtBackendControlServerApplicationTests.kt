be1c587ac9edef9b5b9153f0104a494a66dad833
sangseng <44915367+miche715@users.noreply.github.com>
2025-08-30 22:14:46 +0900

    [backend] chore: 프로젝트 초기화.

---
+++ gt-backend-control-server/src/test/kotlin/com/sangseng/gtbackendcontrolserver/GtBackendControlServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sangseng.gtbackendcontrolserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GtBackendControlServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
